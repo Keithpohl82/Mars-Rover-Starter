8a29471811aa7c6f358fdb7115ecf4a92da2b1a6
keithpohl82 <keith.pohl82@gmail.com>
2024-07-19 09:43:40 -0500

    autograder test

--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -5,7 +5,7 @@ const Command = require('../command.js');
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
-
+// Added this comment for git changes to rerun autograder
 describe("Rover class", function() {
 
   // 7 tests here!
